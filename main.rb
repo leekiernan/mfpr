d161b994f347fe026efd64ac6c8761376e9158c1
Lee Kiernan <dev@starlord.co>
2018-06-05 00:48:25 +0100

    Fix spacing

--- main.rb
+++ main.rb
@@ -1,28 +1,34 @@
-require "./config/capybara";
-require "./config/db";
+#!/bin/bash /usr/local/bin/ruby
 
+require "/app/config/capybara";
+require "/app/config/db";
+
+puts "Running"
 User.all.each do |user|
-	begin
-		browser = Capybara.current_session
-		browser.visit "https://www.myfitnesspal.com/account/login"
+  begin
+    browser = Capybara.current_session
+    browser.visit "https://www.myfitnesspal.com/account/login"
 
-		browser.fill_in "username", with: user.email
-		browser.fill_in "password", with: user.password
-		browser.click_button "Log In"
+    browser.fill_in "username", with: user.email
+    browser.fill_in "password", with: user.password
+    browser.click_button "Log In"
 
-		browser.visit "https://www.myfitnesspal.com/food/quick_add?meal=#{user.meal_to_append}"
+    browser.visit "https://www.myfitnesspal.com/food/quick_add?meal=#{user.meal_to_append}"
+    browser.all(".unit")[0].sibling("input").set "1"
+    browser.click_button "Add to Diary"
 
-		# browser.fill_in "ember1602", with: "1"
-		browser.all(".unit")[0].sibling("input").set "1"
-		browser.click_button "Add to Diary"
+    user.failed_attempts = 0
+  rescue
+    user.failed_attempts = user.failed_attempts + 1
+  end
 
-		user.failed_attempts = 0
-	rescue
-		user.failed_attempts = user.failed_attempts + 1
-	end
-	user.save
-	user.touch
-	sleep 5
-	Capybara.reset_sessions!
+  if user.changed?
+    user.save
+  else
+    user.touch
+  end
+
+  sleep 5
+  Capybara.reset_sessions!
 end
 
