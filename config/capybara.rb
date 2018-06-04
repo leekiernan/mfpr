670a9effd029a48c6c386def75eb120afd403362
Lee Kiernan <dev@starlord.co>
2018-06-05 00:48:09 +0100

    Capybara using less power - useful for containers

--- config/capybara.rb
+++ config/capybara.rb
@@ -1,17 +1,21 @@
 require "capybara"
 require "selenium-webdriver"
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+Capybara.server = :puma, { Silent: true }
+Capybara.register_driver :chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
-
-# Capybara.javascript_driver = :headless_chrome
-# Capybara.javascript_driver = :chrome
-Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.javascript_driver = :chrome_headless
 
 Capybara.configure do |config|
   config.default_max_wait_time = 10
-  config.default_driver    = :selenium_chrome_headless
-  # config.default_driver    = :selenium
+  config.default_driver    = :chrome_headless
 end
